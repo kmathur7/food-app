f4da11ac77b03d46843f01d35206a656d2241766
Kunal Mathur <mathurkunal@gmail.com>
2016-07-25 13:37:20 +0530

    adding styles

--- src/app/+home/fa-home/fa-home.component.ts
+++ src/app/+home/fa-home/fa-home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm }    from '@angular/forms';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { FaHeaderComponent } from '../../shared/fa-header/fa-header.component';
+import { SearchField } from '../search-field';
 
 
 
@@ -9,26 +12,14 @@ import { FaHeaderComponent } from '../../shared/fa-header/fa-header.component';
   selector: 'app-fa-home',
   templateUrl: 'fa-home.component.html',
   styleUrls: ['fa-home.component.css'],
-  directives: [FaHeaderComponent]
+  directives: [FaHeaderComponent, ROUTER_DIRECTIVES]
 
 })
 export class FaHomeComponent implements OnInit {
 
-  lat = 0;
-  long = 0;
-
-  locate() {
-    if ("geolocation" in navigator) {
-      /* geolocation is available */
-      navigator.geolocation.getCurrentPosition( position => {
-        this.lat= position.coords.latitude;
-        this.long = position.coords.longitude;
-      });
-    } else {
-      /* geolocation IS NOT available */
-      console.log("Can't seem to be able to locate you! Try again later.")
-    }
-  }
+  model = new SearchField('',0,0);
+  submitted=false;
+  onSubmit() { this.submitted = true; }
   constructor() {
 
   }
---
+++ src/app/+home/search-field.ts
@@ -0,0 +1,7 @@
+export class SearchField {
+    constructor(
+     public term: string,
+     public latitude?: number,
+     public longitude?: number
+ ) {}  
+}
