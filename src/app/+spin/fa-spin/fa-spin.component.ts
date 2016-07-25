d486d6b8774788157ed055f84dc2b4f381da89fe
Kunal Mathur <mathurkunal@gmail.com>
2016-07-25 15:21:47 +0530

    fixing issues

--- src/app/+spin/fa-spin/fa-spin.component.ts
+++ src/app/+spin/fa-spin/fa-spin.component.ts
@@ -14,6 +14,7 @@ import { FaRestaurantComponent } from '../../shared/fa-restaurant/fa-restaurant.
 })
 export class FaSpinComponent implements OnInit {
   url: string;
+  length: number;
   errorMessage: any;
   showSpinner: boolean = true;
   public restaurants: any[];
